0e34ead8f5e4cf6f0396d49b3e9999d4ec34f9da
Dmitriy Zamulin <zamulin1998@gmail.com>
2022-12-10 15:13:24 +0300

    feat: add basic operations

--- index.js
+++ index.js
@@ -79,6 +79,96 @@ rl.on("line", async (input) => {
       console.log("wrong file");
     }
   }
+  if (input.match(/\badd\b/)) {
+    const fileName = input.split(" ")[1];
+    try {
+      fsPromise.appendFile(fileName, "");
+    } catch (error) {
+      console.log("creating file failed");
+    }
+  }
+  if (input.match(/\brn\b/)) {
+    const oldPath = input.trim().split(" ")[1];
+    const newFileName = input.trim().split(" ")[2];
+    try {
+      fsPromise.rename(
+        oldPath,
+        path.resolve(path.dirname(oldPath), newFileName)
+      );
+    } catch (error) {
+      console.log("renaming was failed");
+    }
+  }
+  if (input.match(/\bcp\b/)) {
+    const oldPath = input.trim().split(" ")[1];
+    let newPathValue = input.trim().split(" ")[2];
+    const newPath = path.isAbsolute(newPathValue)
+      ? newPathValue
+      : path.resolve(currentPath, newPathValue);
+    const fileName = path.basename(oldPath);
+    const readStream = fs.createReadStream(oldPath);
+    const writeStream = fs.createWriteStream(path.join(newPath, fileName));
+    readStream.on("start", () => {
+      fsPromise.appendFile(path.join(newPath, fileName), "");
+    });
+    readStream.on("data", (chunk) => {
+      writeStream.write(chunk);
+    });
+    readStream.on("end", () => {
+      writeStream.end();
+    });
+    readStream.on("error", (error) => {
+      console.log(error);
+    });
+  }
+  if (input.match(/\bmv\b/)) {
+    const pathToFile = input.trim().split(" ")[1];
+    const fileName = path.basename(pathToFile);
+    const newPathValue = input.trim().split(" ")[2];
+    const newPath = path.isAbsolute(newPathValue)
+      ? newPathValue
+      : path.join(currentPath, newPathValue);
+    const readStream = fs.createReadStream(pathToFile);
+    readStream
+      .on("error", (error) => {
+        console.log("------------------------");
+        console.log(error.message);
+        readStream.destroy();
+      })
+      .on("data", (chunk) => {
+        const writeStream = fs.createWriteStream(path.join(newPath, fileName));
+        writeStream.on("error", (error) => {
+          if (error && error.message) {
+            console.log(error.message);
+          }
+          readStream.destroy();
+          writeStream.end();
+        });
+        writeStream.write(chunk);
+      })
+      .on("end", () => {
+        console.log("ended");
+        try {
+          fsPromise.rm(pathToFile);
+        } catch (error) {
+          console.log(error);
+        }
+      })
+      .on("close", (error) => {
+        console.log("destroy");
+      });
+  }
+  if (input.match(/\brm\b/)) {
+    const pathValue = input.trim().split(" ")[1];
+    const pathToFile = path.isAbsolute(pathValue)
+      ? pathValue
+      : path.join(currentPath, pathValue);
+    try {
+      await fsPromise.rm(pathToFile);
+    } catch (error) {
+      console.log("no such a file");
+    }
+  }
   console.log(`You are currently in ${currentPath}`);
 });
 process.on("exit", () => {
